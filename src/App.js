e5be30646bf2b06be45cfa2af73670ee31c2fddc
tmgAzit <azit.ghising@gmail.com>
2022-05-27 09:48:13 +0930

    pagination is complete.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,34 @@ function App() {
   const [page, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
 
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page, data]);
+
   const handlePage = (index) => {
     setPage(index);
   };
 
-  useEffect(() => {
-    if (loading) return;
-    setFollowers(data[page]);
-  }, [loading]);
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
   return (
     <main>
       <div className='section-title'>
@@ -30,17 +50,23 @@ function App() {
           loading
         ) : (
           <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
             {data.map((item, index) => {
               return (
                 <button
                   key={index}
-                  className='page-btn'
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
                   onClick={() => handlePage(index)}
                 >
                   {index + 1}
                 </button>
               );
             })}
+            <but className='next-btn' onClick={nextPage}>
+              next
+            </but>
           </div>
         )}
       </section>
